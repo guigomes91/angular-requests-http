07e921827e82dbdaeaf5cdffd16e299073b0dbe8
Guilherme Gomes <guilherme.gomes91@outlook.com>
2023-07-04 21:58:46 -0300

    Popup de confirmação para remover cursos

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,75 +1,80 @@
-import { Component, OnInit } from '@angular/core';
-import { CursosService } from '../cursos.service';
-import { Curso } from '../curso';
-import { EMPTY, Observable, Subject, catchError } from 'rxjs';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
-import { AlertModalComponent } from 'src/app/shared/alert-modal/alert-modal.component';
-import { AlertModalService } from 'src/app/shared/alert-modal.service';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { catchError, EMPTY, Observable, Subject } from 'rxjs';
+import { AlertModalService } from 'src/app/shared/alert-modal.service';
+
+import { Curso } from '../curso';
+import { CursosService } from '../cursos.service';
 
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
   styleUrls: ['./cursos-lista.component.scss'],
-  preserveWhitespaces: true
+  preserveWhitespaces: true,
 })
 export class CursosListaComponent implements OnInit {
 
-  //cursos!: Curso[];
   bsModalRef?: BsModalRef;
   cursos$!: Observable<Curso[]>;
   error$ = new Subject<boolean>();
+  @ViewChild('deleteModal') deleteModal: any;
 
-  constructor(private service: CursosService,
-    //private modalService: BsModalService) {}
-  private alertService: AlertModalService,
-  private router: Router,
-  private route: ActivatedRoute){}
+  cursoSelecionado!: Curso;
+
+  constructor(
+    private service: CursosService,
+    private modalService: BsModalService,
+    private alertService: AlertModalService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    //this.service.list()
-      //.subscribe(dados => this.cursos = dados);
-
-      this.onRefresh();
+    this.onRefresh();
   }
 
   onRefresh() {
-    this.cursos$ = this.service.list()
-        .pipe(
-          catchError(error => {
-            console.error(error);
-            //this.error$.next(true);
-            this.handleError();
-            return EMPTY;
-          })
-        );
-
-    /*this.service.list()
-    .pipe(
-      catchError(error => EMPTY)
-    )
-    .subscribe(
-      dados => {
-        console.log(dados)
-      },
-      //error => console.log(error),
-      //() => console.log('Observable completo!')
-    );*/
+    this.cursos$ = this.service.list().pipe(
+      catchError((error) => {
+        this.handleError();
+        return EMPTY;
+      })
+    );
   }
 
   handleError() {
-    this.alertService.showAlertDanger('Erro ao carregar cursos. Tenta novamente mais tarde.');
-    //this.bsModalRef = this.modalService.show(AlertModalComponent);
-    //this.bsModalRef.content.type = 'danger';
-    //this.bsModalRef.content.message = 'Erro ao carregar cursos. Tenta novamente mais tarde.';
+    this.alertService.showAlertDanger(
+      'Erro ao carregar cursos. Tenta novamente mais tarde.'
+    );
   }
 
   onEdit(cursoId: number) {
-    this.router.navigate(['editar', cursoId], { relativeTo: this.route});
+    this.router.navigate(['editar', cursoId], { relativeTo: this.route });
   }
 
-  onDelete(curso: any) {
-
+  onDelete(curso: Curso) {
+    this.cursoSelecionado = curso;
+    this.bsModalRef = this.modalService.show(this.deleteModal, {
+      class: 'modal-sm',
+    });
   }
 
+  onConfirmDelete() {
+    this.service.remove(this.cursoSelecionado.id).subscribe(
+      success => {
+        this.alertService.showAlertSuccess('Curso deletado com sucesso!');
+        this.onRefresh();
+        this.bsModalRef?.hide();
+      },
+      error => {
+        this.alertService.showAlertDanger('Erro ao remover curso, tente novamente mais tarde!')
+        this.bsModalRef?.hide();
+      }
+    );
+  }
+
+  onDeclineDelete() {
+    this.bsModalRef?.hide();
+  }
 }
