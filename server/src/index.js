c1e8d63f637b284f49f70ece94361fe3bf90d0f6
Guilherme Gomes <guilherme.gomes91@outlook.com>
2023-07-11 21:42:19 -0300

    Download de arquivo

--- server/src/index.js
+++ server/src/index.js
@@ -4,20 +4,21 @@ const bodyParser = require("body-parser");
 const multipart = require("connect-multiparty");
 
 const app = express();
-//app.arguments(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-/*const corsOptions = {
-  origin: "*",
-  optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));*/
 
 const multipartMiddleware = multipart({ uploadDir: "./uploads" });
 app.post("/upload", multipartMiddleware, (req, res) => {
   const files = req.files;
   console.log(files);
   res.json({ message: files });
+});
+
+app.get('/downloadExcel', (req, res) => {
+  res.download('./uploads/report.xlsx');
+});
+
+app.get('/downloadPDF', (req, res) => {
+  res.download('./uploads/report.pdf');
 });
 
 app.use((err, req, res, next) => res.json({ error: err.message }));
--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { UploadFileService } from '../upload-file.service';
 import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-upload-file',
   templateUrl: './upload-file.component.html',
-  styleUrls: ['./upload-file.component.scss']
+  styleUrls: ['./upload-file.component.scss'],
 })
 export class UploadFileComponent implements OnInit {
-
   files!: Set<File>;
-  progress:number = 0;
+  progress: number = 0;
 
-  constructor(private service: UploadFileService){}
+  constructor(private service: UploadFileService) {}
 
   ngOnInit() {}
 
   onChange(evento: any) {
-    console.log(evento)
+    console.log(evento);
 
     const selectedFile = <FileList>evento.srcElement.files;
     //document.getElementById('customFileLabel')!.innerHTML = selectedFile[0].name;
@@ -29,24 +29,40 @@ export class UploadFileComponent implements OnInit {
       this.files.add(selectedFile[i]);
     }
 
-    document.getElementById('customFileLabel')!.innerHTML = fileNames.join(', ');
+    document.getElementById('customFileLabel')!.innerHTML =
+      fileNames.join(', ');
     this.progress = 0;
   }
 
   onUpload() {
     if (this.files && this.files.size > 0) {
-      this.service.upload(this.files, '/api/upload')
-      .subscribe((event: HttpEvent<Object>) => {
+      this.service
+        .upload(this.files, '/api/upload')
+        .subscribe((event: HttpEvent<Object>) => {
           //HttpEventType
           console.log(event);
           if (event.type === HttpEventType.Response) {
             console.log('Upload concluído!');
           } else if (event.type === HttpEventType.UploadProgress) {
-            const percentDone = Math.round((event.loaded * 100) / (event.total || 1));
+            const percentDone = Math.round(
+              (event.loaded * 100) / (event.total || 1)
+            );
             console.log('Progress', percentDone);
             this.progress = percentDone;
           }
         });
     }
+  }
+
+  onDownloadExcel() {
+    this.service.download('/api/downloadExcel').subscribe((res: any) => {
+      this.service.handleFile(res, 'report.xlsx');
+    });
+  }
+
+  onDownloadPDF() {
+    this.service.download('/api/downloadPDF').subscribe((res: any) => {
+      this.service.handleFile(res, 'report.pdf');
+    });
   }
 }
