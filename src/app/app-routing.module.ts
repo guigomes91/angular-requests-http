817e53ce91e5fd2a78ed4626c96e31482c4fe4aa
Guilherme Gomes <guilherme.gomes91@outlook.com>
2023-07-11 21:58:12 -0300

    Criando tela de pesquisa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'upload' },
+  { path: '', pathMatch: 'full', redirectTo: 'busca-reativa' },
   { path: 'cursos', loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule)},
   { path: 'upload', loadChildren: () => import('./upload-file/upload-file.module').then(m => m.UploadFileModule) },
-  { path: 'rxjs-poc', loadChildren: () => import('./unsubscribe-rxjs/unsubscribe-rxjs.module').then(m => m.UnsubscribeRxjsModule) }
+  { path: 'rxjs-poc', loadChildren: () => import('./unsubscribe-rxjs/unsubscribe-rxjs.module').then(m => m.UnsubscribeRxjsModule) },
+  { path: 'busca-reativa', loadChildren: () => import('./reactive-search/reactive-search.module').then(m => m.ReactiveSearchModule) }
 ];
 
 @NgModule({
